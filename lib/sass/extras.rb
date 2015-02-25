fde6cc5d1e9e22642b68a8d28239919c99d55bc4
Tobias Haagen Michaelsen <tobias.michaelsen@karnovgroup.com>
2015-02-25 15:02:05 +0100

    Workaround for Sass's strange load strategy
    Sass::Script::Color is dynamically assigned to Sass::Script::Value::Color when it's first referenced.
    So our strategy of just opening up a class resulted in errors and warnings.

--- lib/sass/extras.rb
+++ lib/sass/extras.rb
@@ -4,20 +4,15 @@ require "sass/extras/contrast"
 require "sass/extras/inline_color_image"
 require "sass/extras/svg_data_urls"
 require "sass/extras/yuv"
-require "sass/script"
 
-module Sass
-  module Script
-    class Color
-      include Sass::Extras::Contrast::Color
-      include Sass::Extras::YUV::Color
-    end
+Sass::Script::Color.class_eval do
+  include Sass::Extras::Contrast::Color
+  include Sass::Extras::YUV::Color
+end
 
-    module Functions
-      include Sass::Extras::Contrast::Functions
-      include Sass::Extras::InlineColorImage
-      include Sass::Extras::SvgDataUrls
-      include Sass::Extras::YUV::Functions
-    end
-  end
+Sass::Script::Functions.module_eval do
+  include Sass::Extras::Contrast::Functions
+  include Sass::Extras::InlineColorImage
+  include Sass::Extras::SvgDataUrls
+  include Sass::Extras::YUV::Functions
 end
